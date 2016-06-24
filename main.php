4ca0454462251e92d0f08581a6f06fa298fa86cd
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-06-23 22:24:13 -0400

    A beginning of our work ....

---
+++ main.php
@@ -0,0 +1,10 @@
+<meta name="viewport" id="viewport" content="width=device-width,user-scalable=false;">
+<?php
+
+$season = &get_pref('style-season');
+if ( strcmp($season,'default') == 0 )
+{
+  return;
+}
+
+?>
